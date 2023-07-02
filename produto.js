7e37c3ebecea68b0fd29850a3e2974df6d658c38
AlexandreAMMB <31141564+AlexandreAMMB@users.noreply.github.com>
2023-07-02 12:42:53 -0300

    Criação da classe produto e da produtoDAO

---
+++ produto.js
@@ -0,0 +1,10 @@
+class Produto {
+    
+    constructor(codigo, nome, descricao, precoUnit, foto) {
+        this.codigo = codigo
+        this.nome = nome
+        this.descricao = descricao
+        this.precoUnit = precoUnit
+        this.foto = foto
+    }
+}
